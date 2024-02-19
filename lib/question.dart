74d4510623c61acf1530ffc3fbcd1d251bc3e6f3
midhunsaji10 <160301328+midhunsaji10@users.noreply.github.com>
2024-02-19 17:17:37 +0530

    Initial commit

---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Question extends StatefulWidget {
+  const Question({Key? key}) : super(key: key);
+
+  @override
+  State<Question> createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown,
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:quiz/question.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+      Timer(Duration(seconds: 4), () {
+        Navigator.push(context,MaterialPageRoute(builder: (context) => Question(),));
+      });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://thumbs.dreamstime.com/b/male-hand-holding-megaphone-quiz-time-speech-bubble-loudspeaker-banner-business-marketing-advertising-vector-125104939.jpg'),
+              radius: 100,
+            ),
+            SizedBox(height: 20,),
+            Text('GET READY TO FEEL THE THUNDER',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
