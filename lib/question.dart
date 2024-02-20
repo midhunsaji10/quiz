c0484baf2ce1df61f22d0efa70241ebdcdf707ae
midhunsaji10 <160301328+midhunsaji10@users.noreply.github.com>
2024-02-20 17:16:37 +0530

    question created

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 class Question extends StatefulWidget {
   const Question({Key? key}) : super(key: key);
 
@@ -11,6 +13,45 @@ class _QuestionState extends State<Question> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('DO YOU HAVE A BOMB?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 70),),
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              width: 500,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+
+                },
+                child: const Text('YES'),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 500,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+
+                },
+                child: const Text('NO'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+
+
