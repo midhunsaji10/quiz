123ddd3a1a6397b1fdfaf05c512a9af22300e8d7
midhunsaji10 <160301328+midhunsaji10@users.noreply.github.com>
2024-02-29 17:07:03 +0530

    initial commit

--- lib/question.dart
+++ lib/question.dart
@@ -17,9 +17,9 @@ class _QuestionState extends State<Question> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           Text(
+            Text(
               qstn[indx].quiz,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 70),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 70),
             ),
             const SizedBox(
               height: 200,
@@ -29,6 +29,7 @@ class _QuestionState extends State<Question> {
               height: 50,
               child: ElevatedButton(
                 onPressed: () {
+                  check(true);
                   next();
                 },
                 child: const Text('YES'),
@@ -42,35 +43,53 @@ class _QuestionState extends State<Question> {
               height: 50,
               child: ElevatedButton(
                 onPressed: () {
+                  check(false);
                   next();
                 },
                 child: const Text('NO'),
               ),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'RESULT:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Text(
+              result,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            )
           ],
         ),
       ),
     );
   }
-  List qstn=[
-    Myquestions(quiz:'do you have a bomb?',ans: false),
-    Myquestions(quiz:'are you happy?',ans: true),
-    Myquestions(quiz:'does bike have 2 wheels?',ans: true),
-    Myquestions(quiz:'ektha is a genius?',ans: false),
-    Myquestions(quiz:'is judy the cool guy?',ans: true),
-    Myquestions(quiz:'there are 8 planets on solar system?',ans: false),
-    Myquestions(quiz:'are bombs made in kannur?',ans: false),
-    Myquestions(quiz:'computer is a machine?',ans: true),
 
+  List qstn = [
+    Myquestions(quiz: 'do you have a bomb?', ans: false),
+    Myquestions(quiz: 'are you happy?', ans: true),
+    Myquestions(quiz: 'does bike have 2 wheels?', ans: true),
+    Myquestions(quiz: 'ektha is a genius?', ans: false),
+    Myquestions(quiz: 'is judy the cool guy?', ans: true),
+    Myquestions(quiz: 'there are 8 planets on solar system?', ans: false),
+    Myquestions(quiz: 'can you see air?', ans: false),
+    Myquestions(quiz: 'computer is a machine?', ans: true),
   ];
-  int indx=0;
-  void next(){
-    if(indx< qstn.length){
+  int indx = 0;
+  void next() {
+    if (indx < qstn.length+1) {
       indx++;
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
+  String result = '';
+  void check(bool answer) {
+    if (answer == qstn[indx].ans) {
+      result = 'correct answer';
+    } else {
+      result = 'wrong answer';
+    }
+  }
 }
